c582d6e0c055527e372fc2d0d51ab04ba698bcab
Bharti vishwakarma <144533224+BhartiVishwa@users.noreply.github.com>
2025-10-16 20:46:58 +0530

    changes into UserModel

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/config/db";
-import User from "@/lib/models/UserModel";
+import User from "@/lib/models/UserModel.js";
 import jwt from "jsonwebtoken";
 
 export async function POST(request) {
