e99e88ba5157315a240b611b183afd139cbb206c
Bharti vishwakarma <144533224+BhartiVishwa@users.noreply.github.com>
2025-10-16 14:24:52 +0530

    chanes in login , signup, admin route.js

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/config/db";
-import User from "@/lib/models/UserModel";
+import User from "../../../../lib/models/UserModel.js";
 import jwt from "jsonwebtoken";
 
 export async function POST(request) {
@@ -13,12 +13,19 @@ export async function POST(request) {
     }
 
     const user = await User.create({ name, email, password });
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "24hr" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24hr",
+    });
 
-return Response.json({
-  token,
-  user: { id: user._id, name: user.name, email: user.email, role: user.role }
-});
+    return Response.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
     return Response.json({ error: "Signup failed" }, { status: 500 });
   }
